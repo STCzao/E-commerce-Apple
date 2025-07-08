128dd703d0a9ef192b3b5d14c5b8260bd1a4ccd2
STCzao <Santicoronel0183@gmail.com>
2025-07-08 14:21:15 -0300

    Agregamos detalles en el Hero y el componente de Contacto

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,15 +1,35 @@
+import { motion } from "framer-motion";
+
 const Hero = () => {
   return (
     <div>
       <div className="w-full min-h-screen bg-[linear-gradient(rgba(0,0,0,0.7),rgba(0,0,0,0.7)),url(../src/assets/fondo-hero.png)] bg-cover bg-center text-white flex items-center justify-center">
-        <div className="flex flex-col items-center text-white/90 ">
-          <span className="text-5xl font-semibold mb-15">
+        <motion.div
+          initial={{ opacity: 0.0, y: 40 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{
+            delay: 0.3,
+            duration: 0.8,
+            ease: "easeInOut",
+          }}
+          className="flex flex-col items-center text-white/90"
+        >
+          <motion.span className="text-5xl font-semibold mb-15">
             La mejor manera de comprar los productos que amas.
-          </span>
-          <span className="text-xl">
+          </motion.span>
+          <motion.span
+            className="text-xl"
+            initial={{ opacity: 0.0, y: 20 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{
+              delay: 0.5,
+              duration: 0.8,
+              ease: "easeInOut",
+            }}
+          >
             Sumérgete en el mundo Apple
-          </span>
-        </div>
+          </motion.span>
+        </motion.div>
       </div>
     </div>
   );
