80d824de2d13a2729973e016d13579d41576c3c2
STCzao <Santicoronel0183@gmail.com>
2025-07-10 23:02:25 -0300

    Cambios en el texto principal del Hero

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,35 +1,47 @@
+"use client";
+
 import { motion } from "framer-motion";
 
 const Hero = () => {
   return (
     <div>
       <div className="w-full min-h-screen bg-[linear-gradient(rgba(0,0,0,0.7),rgba(0,0,0,0.7)),url(../src/assets/fondo-hero.png)] bg-cover bg-center text-white flex items-center justify-center">
-        <motion.div
-          initial={{ opacity: 0.0, y: 40 }}
-          whileInView={{ opacity: 1, y: 0 }}
-          transition={{
-            delay: 0.3,
-            duration: 0.8,
-            ease: "easeInOut",
-          }}
-          className="flex flex-col items-center text-white/90"
-        >
-          <motion.span className="text-5xl font-semibold mb-15">
-            La mejor manera de comprar los productos que amas.
-          </motion.span>
-          <motion.span
-            className="text-xl"
-            initial={{ opacity: 0.0, y: 20 }}
-            whileInView={{ opacity: 1, y: 0 }}
-            transition={{
-              delay: 0.5,
-              duration: 0.8,
-              ease: "easeInOut",
+        <div className="flex flex-col items-center text-white/90">
+          <h1 className="text-5xl font-semibold mb-15 text-center">
+            {"La mejor manera de comprar los productos que amas."
+              .split(" ")
+              .map((word, index) => (
+                <motion.span
+                  key={index}
+                  initial={{ opacity: 0, filter: "blur(4px)", y: 10 }}
+                  animate={{ opacity: 1, filter: "blur(0px)", y: 0 }}
+                  transition={{
+                    duration: 0.3,
+                    delay: index * 0.1,
+                    ease: "easeInOut",
+                  }}
+                  className="mr-2 inline-block"
+                >
+                  {word}
+                </motion.span>
+              ))}
+          </h1>
+          <motion.p
+            initial={{
+              opacity: 0,
             }}
+            animate={{
+              opacity: 1,
+            }}
+            transition={{
+              duration: 0.3,
+              delay: 0.8,
+            }}
+            className="text-xl"
           >
             Sumérgete en el mundo Apple
-          </motion.span>
-        </motion.div>
+          </motion.p>
+        </div>
       </div>
     </div>
   );
