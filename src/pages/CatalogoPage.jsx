2bf512654f1e68ed3a64c3001aea4db0ff29c4d2
STCzao <Santicoronel0183@gmail.com>
2025-07-11 19:56:08 -0300

    Arrancamos con la pagina del Catalogo

--- src/pages/CatalogoPage.jsx
+++ src/pages/CatalogoPage.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import CatalogoComponente from "../Components/CatalogoComponente/CatalogoComponente";
 
 const CatalogoPage = () => {
   return (
     <div>
+      <div>
+        <CatalogoComponente />
+      </div>
       <h1>Esta es la seccion de catalogo</h1>
       <button>
         <Link to="/">Volver a la pagina principal</Link>
